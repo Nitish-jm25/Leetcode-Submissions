2da88a0f09397b7e37219b08b389a4ac3bb49544
Nitish Raj J M <jmnitishraj@gmail.com>
2026-08-26 14:53:59 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (30.63%) - LeetSync

--- 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.py
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.py
@@ -1,29 +1,19 @@
-class Solution(object):
-    def shortestBeautifulSubstring(self, s, k):
-        if s.count('1') < k:
+class Solution:
+    def shortestBeautifulSubstring(self, s: str, k: int) -> str:
+        if s.count("1")<k:
             return ""
-        
-        left = 0
-        ones_in_window = 0
-        min_length = float("inf")
-        best = ""
+        left,ones,best=0,0,""
+        minlen=float("inf")
 
         for right in range(len(s)):
-            if s[right] == "1":
-                ones_in_window += 1
-            while ones_in_window == k:
-                current_string = s[left:right+1]
-                current_length = len(current_string)
-
-                if current_length < min_length:
-                    min_length = current_length
-                    best = current_string
-                
-                elif current_length == min_length:
-                    best = min(best,current_string)
-            
-                if s[left] == "1":
-                     ones_in_window -= 1
-        
-                left += 1
+            if s[right]=="1":
+                ones+=1
+            while ones==k:
+                curr_str=s[left:right+1]
+                if len(curr_str)<minlen or (curr_str<best and len(curr_str)==minlen):
+                    best = curr_str
+                    minlen=len(curr_str)
+                if s[left]=="1":
+                    ones-=1
+                left+=1
         return best
