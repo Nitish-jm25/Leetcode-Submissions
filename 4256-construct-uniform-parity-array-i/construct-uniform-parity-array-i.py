ccd0eeb4ad5b6d0a4b33f659e6dc6b9fd51d6c1d
Nitish Raj J M <jmnitishraj@gmail.com>
2026-09-02 22:14:42 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (22.50%) - LeetSync

--- 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.py
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.py
@@ -1,9 +1,3 @@
-class Solution:
-    def uniformArray(self, nums1: list[int]) -> bool:
-        nums2 = [0] * len(nums1) 
-        for i in range(len(nums1)):
-            for j in range(len(nums1)):
-                if j!=i:
-                    nums2[i]=nums1[i]-nums1[j]
-                nums2[i]=nums1[i]
+class Solution(object):
+    def uniformArray(self, nums1):
         return True
