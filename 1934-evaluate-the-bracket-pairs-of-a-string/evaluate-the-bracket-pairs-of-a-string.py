c67ea370fd749b27caf4cf3dd7c67df2751f24fe
Nitish Raj J M <jmnitishraj@gmail.com>
2026-09-26 20:10:33 +0530

    Time: 77 ms (53.57%) | Memory: 55.8 MB (82.14%) - LeetSync

--- 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.py
+++ 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.py
@@ -1,21 +1,14 @@
-class Solution:
-    def evaluate(self, s: str, knowledge: list[list[str]]) -> str:
-        key_map = {k:v for k,v in knowledge}
-        output = []
-        current_key = []
-        in_bracket = False
-        for char in s:
-            if char == "(":
-                in_bracket = True
-            elif char == ")":
-                in_bracket = False
-                key_str = "".join(current_key)
-                translate = key_map.get(key_str,"?")
-                output.append(translate)
-                current_key = []
+class Solution(object):
+    def evaluate(self, s, knowledge):
+        key_map = dict(knowledge)
+        res,i = [],0
+
+        while i<len(s):
+            if s[i]=="(":
+                j=s.find(")",i+1)
+                res.append(key_map.get(s[i+1:j],"?"))
+                i=j
             else:
-                if in_bracket:
-                    current_key.append(char)
-                else:
-                    output.append(char)
-        return "".join(output)
+                res.append(s[i])
+            i+=1
+        return "".join(res)
